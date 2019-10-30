939a2eb2afed6becadda7a18234191f837193f35
Dan <dan.chen.auckland@outlook.com>
2019-10-30 20:37:57 +1300

    save changes

--- resource/js/background.js
+++ resource/js/background.js
@@ -1,32 +1,6 @@
 
-// background.js
-/*var jsonProduct = 0;
-chrome.runtime.onMessage.addListener(
-    function(message, sender, sendResponse) {
-        switch(message.type) {
-            case "setProduct":
-                jsonProduct = message.Product;
-                break;
-            case "getProduct":
-                sendResponse(jsonProduct);
-                break;
-            default:
-                console.error("Unrecognised message: ", message);
-        }
-    }
-);
-*/
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-    alert('icon clicked');
     if (tab) {
-        alert('222');
-        chrome.tabs.sendMessage(tab.id, {showProductDiv: true});
+        chrome.tabs.sendMessage(tab.id, {showPopup: true});
     }
 });
-
-/*chrome.browserAction.onClicked.addListener(function(tab) {
-    alert('1111');
-
-
-});*/
--- resource/js/content.js
+++ resource/js/content.js
@@ -1,130 +1,63 @@
-var jquery = '<script src="' + chrome.extension.getURL('resource/js/jquery-3.2.1.min.js') + '" ></script>';
-$('head').append(jquery);
-
-
-
-
-var getProductJson = function() {
-    return {
-        "product": {
-            "name": "",
-            "body_html": "<strong>Good snowboard!</strong>",
-            "vendor": "Burton",
-            "product_type": "Snowboard",
-            "variants": [
-                {
-                    "option1": "Blue",
-                    "option2": "155"
-                },
-                {
-                    "option1": "Black",
-                    "option2": "159"
-                }
-            ],
-            "options": [
-                {
-                    "name": "Color",
-                    "values": [
-                        "Blue",
-                        "Black"
-                    ]
-                },
-                {
-                    "name": "Size",
-                    "values": [
-                        "155",
-                        "159"
-                    ]
-                }
-            ]
-        }
-    };
-};
-
-
-
-
-if (window.location.hostname == "www.aliexpress.com") {
-
-    var name = $(".product-title").length > 0 ? $(".product-title")[0].innerHTML : '';
-
-    var description = $(".product-overview").length  > 0 ? $(".product-overview")[0].innerHTML : '';
-
-    var short_name = '';
-    if ($('span:contains("SPECIFICATIONS")').length > 0) {
-        $('span:contains("SPECIFICATIONS")')[0].click();
-        short_name = ($(".product-specs").length > 0) ? $(".product-specs")[0].innerHTML : '';
-    }
-
-
-
-    var minPrice = '';
-    var maxPrice = '';
-    var price = $(".product-price-value").length  > 0 ? $(".product-price-value")[0].innerHTML : '';
-
-    var color =  $(".sku-property-image").map(function() {
-                    return {'image': $(this).find('img').first().attr('src'), 'color' : $(this).find('img').first().attr('title')};
-                }).get();
-    var images = $(".images-view-item").map(function() {
-                    return $(this).find('img').first().attr('src');
-                }).get();
-
-    var review_star = $(".overview-rating-average").length  > 0 ? $(".overview-rating-average")[0].innerHTML : '';
-    var review_count = $(".product-reviewer-reviews").length  > 0 ? $(".product-reviewer-reviews")[0].innerHTML : '';
-
-    var sold = $(".product-reviewer-sold").length  > 0 ? $(".product-reviewer-sold")[0].innerHTML : '';
-
-    var quantity = $(".product-quantity-tip").length  > 0 ? $(".product-quantity-tip")[0].innerHTML : '';
-
-    var shipping = $(".product-shipping-price").length  > 0 ? $(".product-shipping-price")[0].innerHTML : '';
-
-    var originUrl = window.location;
-    var vendor = $(".shop-name").length  > 0 ? $(".shop-name").find('a').first()[0].innerText : '';
-    var vendorUrl = $(".shop-name").length  > 0 ? $(".shop-name").find('a').first()[0].href : '';
-
-
-    getProductJson = function() {
-        return {
-            "product": {
-                "name": name,
-                "short_description": short_name,
-                "description": description,
-
-                'color' : color,
-                "price": price,
-                'quantity' : quantity,
-
-                "review_star": review_star,
-                "review_count": review_count,
-
-                "sold": sold,
-
-                "images" : images,
-                'shipping' : shipping,
-
-                'vendor' : vendor,
-                'originUrl' : originUrl,
-                'vendorUrl' : vendorUrl
-            }
-        };
-    };
-}
-
-
-function PopupCenter(pageURL, title,w,h) {
-    var left = (screen.width/2)-(w/2);
-    var top = (screen.height/2)-(h/2);
-    var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-
-}
-
-
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-    if(message.showProductDiv == true) {
-        PopupCenter();
+    if(message.showPopup == true) {
+        chrome.storage.sync.get('remoteUrl', function(result) {
+            var url = prompt("Please enter the url to post or an email address", result.remoteUrl);
+            if (url.length > 0) {
+                chrome.storage.sync.set({'remoteUrl': url}, function () {
+                    if (validateEmail(url)) {
+                        window.open('mailto:' + url + '?subject=' + window.location.href + '&body=' + document.documentElement.innerHTML);
+                    } else {
+                        postPageData(url);
+                    }
+
+                });
+            }
+        });
     }
 });
 
+
+function validateEmail(email)
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+
+
+function postPageData(url)
+{
+    var windowHeight = screen.height-200;
+    var windowWidth = screen.width-200;
+    var left= (screen.width/2)-(windowWidth/2);
+    var top = (screen.height/2)-(windowHeight/2);
+
+    window.open("Post Page", "newWindow", 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+windowWidth+', height='+windowHeight+', top='+top+', left='+left);
+
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", url);
+    form.setAttribute("target", "newWindow");
+
+    var hiddenField1 = document.createElement("input");
+    hiddenField1.setAttribute("type", "hidden");
+    hiddenField1.setAttribute("id", "pageUrl");
+    hiddenField1.setAttribute("name", "pageUrl");
+    hiddenField1.setAttribute("value", window.location.href);
+
+    var hiddenField2 = document.createElement("input");
+    hiddenField2.setAttribute("type", "hidden");
+    hiddenField2.setAttribute("id", "pageBody");
+    hiddenField2.setAttribute("name", "pageBody");
+    hiddenField2.setAttribute("value", document.documentElement.innerHTML);
+
+    form.appendChild(hiddenField1);
+    form.appendChild(hiddenField2);
+
+    document.body.appendChild(form);
+    form.submit();
+
+}
 
 
 
